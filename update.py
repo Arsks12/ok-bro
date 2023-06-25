8bb59a277244cfb1b0ea14e52ffa12abc013b867
Arsks12 <101000780+Arsks12@users.noreply.github.com>
2023-06-25 14:44:57 +0530

    Add files via upload

---
+++ update.py
@@ -0,0 +1,66 @@
+from logging import INFO, FileHandler, StreamHandler, basicConfig
+from logging import error as log_error
+from logging import info as log_info
+from os import environ
+from os import path as ospath
+from subprocess import run as srun
+
+from dotenv import load_dotenv
+from requests import get as rget
+
+if ospath.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
+        f.truncate(0)
+
+basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[FileHandler('log.txt'), StreamHandler()],
+                    level=INFO)
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except Exception:
+    UPSTREAM_REPO = None
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except Exception:
+    UPSTREAM_BRANCH = 'main'
+
+if UPSTREAM_REPO is not None:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
+
+    update = srun([f"git init -q \
+                     && git config --global user.email gautampro1834@gmail.com \
+                     && git config --global user.name gautam1834 \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+    if update.returncode == 0:
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    else:
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = rget(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log_error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        log_error(f"CONFIG_FILE_URL: {e}")
+except Exception:
+    pass
+
+load_dotenv('config.env', override=True)
